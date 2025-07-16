b6a92954971f696bfce51843cfadffade4563490
Rishika132 <rishika01.itgeeks@gmail.com>
2025-07-16 17:36:48 +0530

    first commit

--- controller/webhook.controller.js
+++ controller/webhook.controller.js
@@ -3,41 +3,41 @@ const { shopify } = require("../utils/shopify");
 
 const Webhook = async (req, res) => {
   try {
-    const order = req.body;
-    console.log(order);
-       let email;
+     const order = req.body;
+     console.log(order);
+    //    let email;
 
-    for (const item of order.line_items || []) {
-      if (item.properties) {
-        for (const prop of item.properties) {
-          if (prop.name.toLowerCase() === "email" || prop.name.toLowerCase().includes("email")) {
-            email = prop.value;
-            break;
-          }
-        }
-      }
-      if (email) break;
-    }
+    // for (const item of order.line_items || []) {
+    //   if (item.properties) {
+    //     for (const prop of item.properties) {
+    //       if (prop.name.toLowerCase() === "email" || prop.name.toLowerCase().includes("email")) {
+    //         email = prop.value;
+    //         break;
+    //       }
+    //     }
+    //   }
+    //   if (email) break;
+    // }
 
-    // 🔁 If not found in line_items, fallback to order.email
-    if (!email) {
-      email = order.email;
-    }
+    // // 🔁 If not found in line_items, fallback to order.email
+    // if (!email) {
+    //   email = order.email;
+    // }
 
-    if (!email) {
-      return res.status(400).json({ message: "Email not found in order or line items." });
-    }
+    // if (!email) {
+    //   return res.status(400).json({ message: "Email not found in order or line items." });
+    // }
 
-    const existingCustomers = await shopify.customer.search({ query: `email:${email}` });
+    // const existingCustomers = await shopify.customer.search({ query: `email:${email}` });
 
-    if (existingCustomers.length > 0) {
-      console.log(`👤 Customer already exists: ${email}`);
-      return res.status(200).json({ message: "Customer already exists", email });
-    }
+    // if (existingCustomers.length > 0) {
+    //   console.log(`👤 Customer already exists: ${email}`);
+    //   return res.status(200).json({ message: "Customer already exists", email });
+    // }
  
-    const createdCustomer = await shopify.customer.create(email);
+    // const createdCustomer = await shopify.customer.create(email);
 
-    console.log(`✅ Customer created: ${createdCustomer.id} (${email})`);
+    // console.log(`✅ Customer created: ${createdCustomer.id} (${email})`);
     return res.status(200).json({ message: "Customer created", email });
 
   } catch (err) {
