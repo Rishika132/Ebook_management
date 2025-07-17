c39308c7e635151faed23216639c82aff8c04c31
Rishika132 <rishika01.itgeeks@gmail.com>
2025-07-17 16:37:53 +0530

    first commit

--- controller/book.controller.js
+++ controller/book.controller.js
@@ -3,15 +3,15 @@ const Book = require('../model/book.model');
 
 const uploadPdf = async (req, res) => {
   try {
-     const {product_id}=req.body;
+    console.log(req.body)
     if (!req.file) return res.status(400).json({ message: 'No PDF uploaded' });
 
     const fileUrl = `${req.protocol}://${req.get('host')}/uploads/${req.file.filename}`;
     
-    const newBook = new Book({ url: fileUrl , product_id});
+    const newBook = new Book({ url: fileUrl });
     await newBook.save();
 
-    res.status(200).json({ message: 'PDF uploaded & URL saved.',   id: newBook._id ,fileUrl,product_id  });
+    res.status(200).json({ message: 'PDF uploaded & URL saved.',   id: newBook._id ,fileUrl  });
   } catch (err) {
     console.error(err);
     res.status(500).json({ message: 'Internal Server Error' });
