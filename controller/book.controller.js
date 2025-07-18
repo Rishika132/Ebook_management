9155f44c65afc44dbf92aa7a4082c3958c317b37
Rishika132 <rishika01.itgeeks@gmail.com>
2025-07-18 11:18:56 +0530

    first commit

--- controller/book.controller.js
+++ controller/book.controller.js
@@ -1,21 +1,40 @@
-
 const Book = require('../model/book.model');
 
 const uploadPdf = async (req, res) => {
   try {
-    console.log(req.body)
+    console.log(req.body);
+
     if (!req.file) return res.status(400).json({ message: 'No PDF uploaded' });
 
     const fileUrl = `${req.protocol}://${req.get('host')}/uploads/${req.file.filename}`;
-    
-    const newBook = new Book({ url: fileUrl });
+
+    const { product_id } = req.body;
+    if (!product_id) return res.status(400).json({ message: 'Product ID missing' });
+
+    const newBook = new Book({
+      url: fileUrl,
+      product_id: product_id, 
+    });
+
     await newBook.save();
 
-    res.status(200).json({ message: 'PDF uploaded & URL saved.',   id: newBook._id ,fileUrl  });
+    res.status(200).json({
+      message: '✅ PDF uploaded & product ID saved',
+      id: newBook._id,
+      fileUrl,
+    });
   } catch (err) {
     console.error(err);
     res.status(500).json({ message: 'Internal Server Error' });
   }
 };
 
-module.exports = { uploadPdf };
+const product= async(req,res)=>{
+ Book.find()
+    .then(result=>{
+        return res.status(200).json({products:result});
+    }).catch(err =>{
+        return res.status(500).json({error: "Internal Server Error"});
+    });
+}
+module.exports = { uploadPdf,product };
--- routes/book.route.js
+++ routes/book.route.js
@@ -1,11 +1,12 @@
 const express = require("express");
 const upload = require('../middleware/upload');
-const {uploadPdf}  = require("../controller/book.controller");
+const {uploadPdf,product}  = require("../controller/book.controller");
 
 const router = express.Router();
 
 //http://localhost:3000/upload-pdf
 
 router.post('/upload-pdf', upload.single('file'), uploadPdf);
+router.get("/get-url",product);
 
 module.exports = router;
