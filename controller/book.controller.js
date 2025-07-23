eb6f672741ff77794df00cc0d689970c85b1757b
Rishika132 <rishika01.itgeeks@gmail.com>
2025-07-23 16:42:24 +0530

    first commit

--- controller/book.controller.js
+++ controller/book.controller.js
@@ -1,16 +1,20 @@
 const Book = require('../model/book.model');
 const uploadPdf = async (req, res) => {
   try {
+     const { file_name, product_title,size} = req.body;
     if (!req.file) return res.status(400).json({ message: 'No PDF uploaded' });
     const fileUrl = `${req.protocol}://${req.get('host')}/uploads/${req.file.filename}`;
     const newBook = new Book({
-      url: fileUrl
+      url: fileUrl,
+      file_name,
+      product_title,
+      size
     });
     await newBook.save();
     res.status(200).json({
       message: ' PDF uploaded & product ID saved',
       id: newBook._id,
-      fileUrl,
+      fileUrl,file_name,product_title,size
     });
   } catch (err) {
     res.status(500).json({ message: 'Internal Server Error' });
--- model/book.model.js
+++ model/book.model.js
@@ -1,9 +1,22 @@
 const mongoose = require("mongoose");
 const bookSchema = new mongoose.Schema({
+     file_name:{
+         type: String,
+        required: true
+     },
     url: {
         type: String,
         required: true
+     },
+     product_title:{
+         type: String,
+        required: true
+     },
+     size:{
+         type: String,
+        required: true
      }
+    
 });
 const Book = mongoose.model("book", bookSchema);
 module.exports = Book;
